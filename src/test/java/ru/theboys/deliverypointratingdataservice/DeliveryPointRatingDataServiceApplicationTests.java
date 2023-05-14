7f14b98bb0855f11dba5758a9ff9a2468c92d0a6
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-15 00:22:59 +0300

    add Spring project template

---
+++ src/test/java/ru/theboys/deliverypointratingdataservice/DeliveryPointRatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointratingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPointRatingDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
