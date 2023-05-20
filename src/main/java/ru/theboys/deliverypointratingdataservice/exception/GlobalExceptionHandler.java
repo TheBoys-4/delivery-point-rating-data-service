86dccaef03c5caeab4ed2ac0276da40cfac3abb7
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-20 23:14:38 +0300

    rename class

--- src/main/java/ru/theboys/deliverypointratingdataservice/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/theboys/deliverypointratingdataservice/exception/GlobalExceptionHandler.java
@@ -7,23 +7,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.theboys.deliverypointratingdataservice.constants.GlobalExceptionHandlerMessageConstants;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleException(Exception e) {
-        log.error(ExceptionMessageConstants.BASE_MESSAGE, e);
+        log.error(GlobalExceptionHandlerMessageConstants.BASE_MESSAGE, e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorMessage(ExceptionMessageConstants.BASE_MESSAGE + e.getMessage()));
+                .body(new ErrorMessage(GlobalExceptionHandlerMessageConstants.BASE_MESSAGE + e.getMessage()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorMessage> handleEntityNotFoundException(EntityNotFoundException e) {
-        log.error(ExceptionMessageConstants.ENTITY_NOT_FOUND_MESSAGE, e);
+        log.error(GlobalExceptionHandlerMessageConstants.ENTITY_NOT_FOUND_MESSAGE, e);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorMessage(ExceptionMessageConstants.ENTITY_NOT_FOUND_MESSAGE + e.getMessage()));
+                .body(new ErrorMessage(GlobalExceptionHandlerMessageConstants.ENTITY_NOT_FOUND_MESSAGE + e.getMessage()));
     }
 }
