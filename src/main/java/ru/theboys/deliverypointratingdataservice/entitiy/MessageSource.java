97f8b23118650690d1e7d6dc69605748877e30dc
Mikhail Savin <58307835+savin000@users.noreply.github.com>
2023-05-20 14:35:34 +0300

    Update MessageSource.java

--- src/main/java/ru/theboys/deliverypointratingdataservice/entitiy/MessageSource.java
+++ src/main/java/ru/theboys/deliverypointratingdataservice/entitiy/MessageSource.java
@@ -8,13 +8,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "message-source")
+@Table(name = "message_source")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class MessageSource extends BaseModel{
-
+public class MessageSource extends BaseModel {
     private String name;
-
 }
