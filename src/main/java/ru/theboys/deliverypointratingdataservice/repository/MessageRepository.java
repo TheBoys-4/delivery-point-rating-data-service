d50ae927009f949163190acf00d40104f7b71c7c
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-20 17:36:30 +0300

    add Repository annotation

--- src/main/java/ru/theboys/deliverypointratingdataservice/repository/MessageRepository.java
+++ src/main/java/ru/theboys/deliverypointratingdataservice/repository/MessageRepository.java
@@ -1,7 +1,9 @@
 package ru.theboys.deliverypointratingdataservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.theboys.deliverypointratingdataservice.entity.Message;
 
-public interface CommentRepository extends JpaRepository<Message,String> {
+@Repository
+public interface MessageRepository extends JpaRepository<Message,String> {
 }
