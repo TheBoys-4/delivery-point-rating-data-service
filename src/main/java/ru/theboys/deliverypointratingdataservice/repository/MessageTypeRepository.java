87286e81453fa58c02d4fb5ba7d8162cd6359367
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-27 22:57:04 +0300

    update

--- src/main/java/ru/theboys/deliverypointratingdataservice/repository/MessageTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.theboys.deliverypointratingdataservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.theboys.deliverypointratingdataservice.entity.MessageType;
-@Repository
-public interface MessageTypeRepository extends JpaRepository<MessageType,String> {
-}
