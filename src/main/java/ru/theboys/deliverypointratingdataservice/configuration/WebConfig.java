b5e5d3750b6b9c14abde9910dee3b73249db9814
savin000 <58307835+savin000@users.noreply.github.com>
2023-05-28 15:28:28 +0300

    rollback

--- src/main/java/ru/theboys/deliverypointratingdataservice/configuration/WebConfig.java
+++ src/main/java/ru/theboys/deliverypointratingdataservice/configuration/WebConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig{
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
-            }
-        };
-    }
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(final CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+//            }
+//        };
+//    }
 }
